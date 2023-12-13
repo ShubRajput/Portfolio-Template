8ba2b740e0d7c60c48e9ec23e3fc68048a01943f
ShubRajput <shubhamrajput252000@gmail.com>
2023-12-13 19:59:59 +0530

    adding resume and changing logo

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'resume link', //paste the link to your resume here
+                  'https://drive.google.com/file/d/1301lgjvlcp-l9uOXGiCghqA7LrAZLVVJ/view', //paste the link to your resume here
                   '_blank'
                 )
               }
